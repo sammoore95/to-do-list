7b53fedfd3e8d06ee25f1746b17dfb116237779e
Sam Moore <samhmoore95@gmail.com>
2025-09-10 15:47:18 -0400

    feat: Add new function to display user menu

--- to-do-list.py
+++ to-do-list.py
@@ -1,5 +1,15 @@
 
+def print_options():
+    print("""
+          What would you like to do? 
+          1. View List
+          2. Add To List
+          3. Remove Item From List
+          4. Exit""")
+
 def to_do_list():
     print("Welcome to your to-do-list!")
-    to_do_item = input("What would you like to add to the list? ")
+
+
+    
 
