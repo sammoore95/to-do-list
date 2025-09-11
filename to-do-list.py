8f976ae4fd37aded9ae4b67170dbf98e2f4add81
Sam Moore <samhmoore95@gmail.com>
2025-09-11 11:14:14 -0400

    feat: Add functions to check for and remove items from the to-do list

--- to-do-list.py
+++ to-do-list.py
@@ -7,8 +7,9 @@ def print_options():
           3. Remove Item From List
           4. Exit""")
 
-# Helper function to get valid integer input from user
+
 def get_int(prompt):
+    """Helper function to get valid integer input from user"""
     while True:
         try:
             num_of_items = int(input(prompt))
@@ -21,8 +22,28 @@ def get_int(prompt):
     return num_of_items
 
 
+def check_for_item(item):
+    """Helper function that returns True if item is in list, False if item not in list"""
+    with open("to-do-list.txt", "r") as f:
+        list_items = [line.strip().lower() for line in f]
+        if item.strip().lower() in list_items:
+            return True
+        else:
+            return False
+
+
+def remove_item(item):
+    """HelperRemove item from list"""
+    with open("to-do-list.txt", "r") as f:
+        lines = f.readlines()
+    with open("to-do-list.txt", "w") as f:
+        for line in lines:
+            if line.strip().lower() != item.strip().lower():
+                f.write(line)
+
+
 def add_to_list():
-    # Keep asking until the user enters a valid integer using helper function
+    """Adds item(s) to list"""
     num_of_items = get_int("How many items would you like to add to the list? ")
     
     # Adds n items to list
@@ -30,3 +51,20 @@ def add_to_list():
         list_item = input("What would you like to add to the list? ")
         with open("to-do-list.txt", "a") as f:
             f.write(f"{list_item}\n")
+
+
+def remove_from_list():
+    """Removes item(s) from list"""
+    num_of_items = get_int("How many items would you like to remove from the list? ")
+    
+    # removes n items from list
+    for i in range(num_of_items):
+        while True:
+                item = input("What item would you like to remove from the list? ")
+                if check_for_item(item) == True:
+                    remove_item(item)
+                    print(f"{item} has been removed from the list!")
+                    break
+                else:
+                    print("That item is not in the list, please enter an item in the list.")
+                    
